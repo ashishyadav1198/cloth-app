7449a55040546aa0c7fe25df1fcf6bdac80aac45
Ashish Kumar Yadav <ashishyadav2722@gmail.com>
2020-12-02 23:58:11 +0530

    updating our app with routing , collection-preview, collection-item and ShopPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import Homepage from './pages/homepage/Homepage'
 import './App.css'
+import ShopPage from './pages/shop/Shop';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
