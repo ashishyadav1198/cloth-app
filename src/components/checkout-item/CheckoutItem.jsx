0ad98d59d851890e01cbcf66af5295b7ff58fabb
Ashish Kumar Yadav <ashishyadav2722@gmail.com>
2020-12-13 13:52:52 +0530

    added add item and remove item functionality to checkout item component

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import './CheckoutItem.scss'
+import { connect } from "react-redux";
+import {
+  addItem,
+  clearItemFromCart,
+  removeItem,
+} from "../../redux/cart/cart.actions";
 
-function CheckoutItem({ cartItem }) {
+import "./CheckoutItem.scss";
+
+function CheckoutItem({ cartItem, clearItem, addItem, removeItem }) {
   const { name, price, quantity, imageUrl } = cartItem;
   return (
     <div className="checkout-item">
@@ -9,11 +16,25 @@ function CheckoutItem({ cartItem }) {
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <span className="remove-button">X</span>
+      <span className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </span>
     </div>
   );
 }
 
-export default CheckoutItem
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,14 +1,28 @@
 export const addItemToCart = (existingItems, item) => {
-    let duplicateItem = existingItems.find( itemObj => itemObj.id === item.id)
+  let duplicateItem = existingItems.find((itemObj) => itemObj.id === item.id);
 
-    if (duplicateItem) {
-        return existingItems.map( itemObj => {
-            if (itemObj.id === duplicateItem.id) {
-                return {...itemObj,quantity: itemObj.quantity + 1}
-            } 
-            return itemObj
-        })
+  if (duplicateItem) {
+    return existingItems.map((itemObj) => {
+      if (itemObj.id === duplicateItem.id) {
+        return { ...itemObj, quantity: itemObj.quantity + 1 };
+      }
+      return itemObj;
+    });
+  }
+
+  return [...existingItems, { ...item, quantity: 1 }];
+};
+
+export const removeItemFromCart = (existingItems, item) => {
+  const updatedCartItems = [];
+  for (let cartItem of existingItems) {
+    if (cartItem.id === item.id) {
+      if (item.quantity > 1) {
+        updatedCartItems.push({ ...item, quantity: item.quantity - 1 });
+      }
+    } else {
+      updatedCartItems.push(cartItem);
     }
-
-    return [...existingItems, {...item, quantity: 1}]
-}
+  }
+  return updatedCartItems;
+};
